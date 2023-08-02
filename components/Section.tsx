03767fb25601759a4ae6b0c1301d68008b3a34ae
lunarmoon <49crehbgr@gmail.com>
2023-08-02 17:49:25 +0900

    refactor: 부모-자식 props 전달 삭제

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,13 +1,17 @@
-'use client'
+'use client';
 
-import React from 'react';
-import Header from './Header';
+import React, { useEffect, useState } from 'react';
 import TodayWeather from './TodayWeather';
 import HourlyWeather from './HourlyWeather';
 import WeekWeather from './WeekWeather';
 import DetailWeather from './DetailWeather';
 import Footer from './Footer';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
+import Modal from './Modal';
+
+import { BsCardList } from '@react-icons/all-files/bs/BsCardList';
+import { AiOutlineUnorderedList } from '@react-icons/all-files/ai/AiOutlineUnorderedList';
+import { ImMap2 } from '@react-icons/all-files/im/ImMap2';
 type Props = {};
 
 export default function Section({
@@ -19,9 +23,39 @@ export default function Section({
   hourlyWeathers: any;
   weekWeathers: any;
 }) {
+  const [showModal, setShowModal] = useState(false);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+    console.log(windowSize);
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowSize]);
+
   return (
-    <motion.div>
-      <Header />
+    <motion.div
+      initial={{ x: 0 }}
+      animate={showModal ? { x: 0 } : { x: 0 }}
+      exit={{ x: 0 }}
+      transition={{ duration: 1 }}
+      className='relative w-full'
+    >
+      <div className='hidden lg:block lg:pl-5 pt-5 text-white '>
+        <div
+          className='cursor-pointer'
+          onClick={() => setShowModal((prev) => !prev)}
+        >
+          <BsCardList size={30} />
+        </div>
+      </div>
+
       {/* Today Weather */}
       <section id='today' className='snap-start scroll-my-10 my-10'>
         <TodayWeather promise={todayWeathers} other={weekWeathers} />
@@ -43,7 +77,26 @@ export default function Section({
         </section>
       </section>
       {/* Footer */}
-      <Footer />
+      <div className='sticky bottom-0 flex justify-between items-center z-20 bg-slate-400 text-2xl px-5 py-2 lg:mt-5 lg:hidden'>
+        <div className='cursor-pointer'>
+          <ImMap2 size={23} />
+        </div>
+        <div>슬라이더</div>
+        <div className='cursor-pointer' onClick={() => setShowModal(true)}>
+          <AiOutlineUnorderedList size={25} />
+        </div>
+      </div>
+
+      <AnimatePresence>
+        {showModal && (
+          <Modal
+            onClose={() => {
+              setShowModal(false);
+            }}
+            windowSize={windowSize}
+          />
+        )}
+      </AnimatePresence>
     </motion.div>
   );
 }
