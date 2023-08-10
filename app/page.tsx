d027f9f51726283150e85da8a2946048c69cc5e6
lunarmoon7 <49crehbgr@gmail.com>
2023-08-10 16:12:27 +0900

    chore: 사용하지 않는 import 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,10 @@
-import DetailWeather from '@/components/DetailWeather';
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import HourlyWeather from '@/components/HourlyWeather';
-import Modal from '@/components/Modal';
 import Section from '@/components/Section';
-import Splitter from '@/components/Splitter';
-import TodayWeather from '@/components/TodayWeather';
-import { ListItem } from '@/components/WeatherItem';
-import WeekWeather from '@/components/WeekWeather';
-import { AnimatePresence } from 'framer-motion';
-import { useResizable } from 'react-resizable-layout';
 
 export default async function Home() {
   const todayWeathers = await getTodayWeather();
   const hourlyWeathers = await getHourlyWeather();
   const weekWeathers = await getWeekWeather();
+  let savedCities: any[] | null = null;
 
   return (
     <div className='w-screen h-screen relative text-black z-0 snap-y snap-mandatory overflow-y-scroll bg-slate-400 lg:flex '>
